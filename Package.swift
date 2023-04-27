b138050ca5e85fc989c36b00c01f051b298d9b03
Ozbern <ozbern@yandex.ru>
2023-04-27 18:32:52 +0500

    replace file

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DSPhotoEditorSDK",
-            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.5/DSPhotoEditorSDK.xcframework.zip",
-            checksum: "ef389586c74b2122f29c61cb334caf81de9ccba0e3fd0ef8a4a572c681cdd557"),
+            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.6/DSPhotoEditorSDK.xcframework.zip",
+            checksum: "2b6dc7c4fd0df29735e1851b6cae7ace01efe012b6b80a2ad274535a4ea647e4"),
     ]
 )

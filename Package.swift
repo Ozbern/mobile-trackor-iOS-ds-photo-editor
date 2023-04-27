72007f193985c7da1892e83a7a1fb12bdbe3fecd
Ozbern <ozbern@yandex.ru>
2023-04-27 15:30:53 +0500

    Fix url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DSPhotoEditorSDK",
-            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.2/DSPhotoEditorSDK.xcframework.zip",
+            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.3/DSPhotoEditorSDK.xcframework.zip",
             checksum: "ef389586c74b2122f29c61cb334caf81de9ccba0e3fd0ef8a4a572c681cdd557"),
     ]
 )

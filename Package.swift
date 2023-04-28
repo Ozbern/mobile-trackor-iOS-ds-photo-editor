0b8f2f09016bb2682412528ac6d00fff4a3a67f3
Ozbern <ozbern@yandex.ru>
2023-04-28 14:47:54 +0500

    dynamic lib

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DSPhotoEditorSDK",
-            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.9/DSPhotoEditorSDK.xcframework.zip",
-            checksum: "6673859f802c9ab8ae8d6bef2a5184c237198e22f28e8c779f63b89b143a3e8b"),
+            url: "https://github.com/Ozbern/mobile-trackor-iOS-ds-photo-editor/releases/download/2.3.10/DSPhotoEditorSDK.xcframework.zip",
+            checksum: "270acb4beb5e3c0f86c4ce31cdedb9d1f3c4dea055a8ad0cfa49cf589d7835ad"),
     ]
 )
